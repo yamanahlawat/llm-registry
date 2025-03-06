7045dc93547327bc8cae7e544cbf067474e99057
Yaman Ahlawat <yaman@outlook.com>
2025-03-06 22:16:26 +0530

    bulid: bump v0.3.0 to v0.3.1

--- src/llm_registry/__init__.py
+++ src/llm_registry/__init__.py
@@ -7,7 +7,7 @@ from .registry import CapabilityRegistry
 from .repository import CapabilityRepository
 from .utils import create_model_capability
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 __all__ = [
     "ModelCapabilities",
